01d8699d10803e5ef79d8251242c4aec98aa765a
MesSer1024 <messer_@hotmail.com>
2022-12-04 22:24:44 +0100

    adding day template

---
+++ Source/Day5.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace advent_of_code_csharp.Source
+{
+    public class Day5 : IDay
+    {
+        public string Identifier => "day5";
+
+        public bool SameData => true;
+
+        private int ParseFirst(string line)
+        {
+            return 0;
+        }
+
+        private int ParseSecond(string line)
+        {
+            return 0;
+        }
+
+        public void ProcessExample()
+        {
+            var input = @"".Split(Environment.NewLine);
+
+            int result = 0;
+            int result2 = 0;
+
+            foreach(var line in input)
+            {
+                result += ParseFirst(line);
+            }
+
+            foreach(var line in input)
+            {
+                result2 += ParseSecond(line);
+            }
+
+            Assert.AreEqual(0, result);
+            Assert.AreEqual(0, result2);
+        }
+
+        public void ProcessFirst(string[] input)
+        {
+            int result = 0;
+
+            foreach(var line in input)
+            {
+                result += ParseFirst(line);
+            }
+
+            Console.WriteLine($"{Identifier} result is {result} ");
+            Assert.AreEqual(0, result);
+        }
+
+        public void ProcessSecond(string[] input)
+        {
+
+            int result = 0;
+
+            foreach(var line in input)
+            {
+                result += ParseSecond(line);
+            }
+
+            Console.WriteLine($"{Identifier} result is {result} ");
+            Assert.AreEqual(0, result);
+        }
+    }
+}
